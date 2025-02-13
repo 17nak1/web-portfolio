545c56c9985b28ec315cb7d54a1cae875c49d730
17nak1 <akhavan.nazila@gmail.com>
2025-02-13 12:48:48 -0500

    Add VideoSection to the components and add pages folder

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoSection from '../components/VideoSection';
+
+function Home () {
+  return (
+    <>
+      <VideoSection/>
+    </>
+  )
+}
+
+export default Home;
