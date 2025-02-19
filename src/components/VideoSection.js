60de5554c1d437e30e6c32fa071010432daddd59
17nak1 <akhavan.nazila@gmail.com>
2025-02-19 14:01:34 -0500

    Add TypeAnimation to the home page

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './VideoSection.css';
+import { TypeAnimation } from 'react-type-animation';
+import { Button } from './Button';
 
 function VideoSection() {
   return (
     <div className='video-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className='video-btns'>
+      <div class="text-column">
+        <TypeAnimation
+          sequence={[
+            'Hi! I’m Nazila.',
+            1500, // wait for 1 second
+            'I’m a Software Developer.',
+            1500, // wait for 1 second
+            'Building innovative solutions.',
+            1500, // wait for 1 second
+          ]}
+          speed={40}
+          wrapper="span"
+          repeat={Infinity}
+          cursor={true}
+          style={{ fontSize: '2em', color:'rgb(74 81 106)'}}
+        />
+        <div className='aboutme-btns'>
         <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-          GET STARTED
+          More About Me
         </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+      </div>
+      </div>
+      <div class="image-column">
+        <img  alt='' src='/images/woman-coder.jpg'/>
+        {/* <p class="image-credit">
+          Image by <a href="https://www.freepik.com/free-vector/cute-girl-hacker-operating-laptop-cartoon-vector-icon-illustration-people-technology-isolated-flat_65309450.htm" target="_blank" rel="noopener noreferrer">Freepik</a>
+        </p> */}
       </div>
     </div>
   );
