125a76a2aa2ef36dd08d181f9257f8ef94b88603
17nak1 <akhavan.nazila@gmail.com>
2025-02-20 12:20:02 -0500

    Update home page with Footer

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='container'>
+      <p>I’m a Software Developer with a strong background in Mathematics, based in Kingston, ON, Canada. Over the past few years, I’ve had the opportunity to work on a diverse range of projects, developing scalable web applications, designing innovative software solutions, and helping teams drive technical excellence.
+
+My career has been shaped by my love for problem-solving and continuous learning. I specialize in building modern, maintainable systems using a mix of technologies like Ruby on Rails, React.js, and Node.js, and I’m passionate about creating intuitive user experiences, optimizing back-end processes, and ensuring high-quality code through test-driven development.
+
+Throughout my journey, I’ve worked on everything from building RESTful APIs and database management to designing cloud-based deployments and event-driven architectures. I believe in the power of collaboration and thrive in agile environments, where I can both learn from others and share my knowledge to help teams grow.
+
+When I’m not coding, you’ll likely find me diving into new math concepts, exploring the latest tech trends, or brainstorming creative ways to solve complex challenges.
+
+Feel free to check out my projects and get in touch if you’d like to collaborate or just chat about tech!</p>
+    </div>
+  )
+}
+
+export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VideoSection from '../components/VideoSection';
+import Footer from '../components/Footer';
 
 function Home () {
   return (
     <>
       <VideoSection/>
+      <Footer/>
     </>
   )
 }
