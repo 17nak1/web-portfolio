53f27f9472ab64d76bf5155c32bebf6886da1286
17nak1 <akhavan.nazila@gmail.com>
2025-02-21 11:08:02 -0500

    Remove Link to update the App.js with anchors

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import Navbar from'./components/Navbar.js';
 import Home from './pages/Home.js';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Navbar />
+        <section id='home'>
+          <Home />
+        </section>
+        <div className='gradient-line'></div>
+        <section id='about'>
+          <About />
+        </section>
+        <div className='gradient-line'></div>
+        <section id='contact'>
+          <Contact />
+        </section>
       </Router>
     </>
   );
