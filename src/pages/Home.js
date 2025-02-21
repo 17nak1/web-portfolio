33c22e1392be70f351d8e77765d6deebd37853ae
17nak1 <akhavan.nazila@gmail.com>
2025-02-21 11:17:44 -0500

    REmove unused libraries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import VideoSection from '../components/VideoSection';
-// import Footer from '../components/Footer';
-import { useSpring, animated } from 'react-spring';
 
 function Home () {
-  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
-    <animated.div style={props}>
       <VideoSection/>
-      {/* <Footer/> */}
-    </animated.div>
   );
-  // return (
-  //   <>
-  //     <VideoSection/>
-  //     <Footer/>
-  //   </>
-  // )
 }
 
 export default Home;
