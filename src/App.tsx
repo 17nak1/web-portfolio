bc252019a2dd7becfc6f2b3c3406038518a98129
17nak1 <akhavan.nazila@gmail.com>
2025-02-26 11:19:35 -0500

    Update index and App to use TypeSrcipt

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Navbar />
+      <section id='home'>
+        <Home />
+      </section>
+      <div className='gradient-line'></div>
+      <section id='about'>
+        <About />
+      </section>
+      <div className='gradient-line'></div>
+      <section id='contact'>
+        <Contact />
+      </section>
+    </Router>
+  );
+}
+
+export default App;
