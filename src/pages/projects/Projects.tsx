25693d38709a02863ec24b559390b64cfbb08b81
17nak1 <akhavan.nazila@gmail.com>
2025-04-08 09:44:40 -0400

    Add a new component IconSection for better scalability

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import './Projects.css';
+import './Projects.css'
+import IconSection from '../../components/iconSection/IconSection';
 
-const Projects: React.FC = () => {
-  const [activeIcon, setActiveIcon] = useState<number | null>(null);
-  const [iconIndex, setIconIndex] = useState(0);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setActiveIcon(iconIndex);
-      setTimeout(() => setActiveIcon(null), 10000); // Reset after 10 second
-      setIconIndex((prevIndex) => (prevIndex + 1) % 7);
-    }, 1000); // Change icon every second
-
-    return () => clearInterval(intervalId);
-  }, [iconIndex]);
-
+function Projects() {
   return (
-    <div className="stack-container">
-      <div className="icon-circle">
-        <i className={`ci ci-react ci-3x ${activeIcon === 0 ? 'active' : ''}`}></i>
-        <i className={`ci ci-nodejs ci-3x ${activeIcon === 1 ? 'active' : ''}`}></i>
-        <i className={`ci ci-express ci-3x ${activeIcon === 2 ? 'active' : ''}`}></i>
-        <i className={`ci ci-mysql ci-3x ${activeIcon === 3 ? 'active' : ''}`}></i>
-        <i className={`ci ci-typescript ci-3x ${activeIcon === 4 ? 'active' : ''}`}></i>
-        <i className={`ci ci-javascript ci-3x ${activeIcon === 5 ? 'active' : ''}`}></i>
-        <i className={`ci ci-ruby ci-3x ${activeIcon === 6 ? 'active' : ''}`}></i>
+    <div className="abt-container">
+      <div className="abt-text-column">
+        <p>
+          I'm a Software Developer with a strong foundation in Mathematics, based in Kingston, ON, Canada. Over the years, I've had the privilege of working on a wide variety of projects, from developing scalable web applications to designing innovative software solutions that drive technical excellence.
+        </p>
+        
+      </div>
+      <div className="abt-image-column">
+        <IconSection />
       </div>
     </div>
   );
-};
+}
 
-export default Projects;
+
+export default Projects
